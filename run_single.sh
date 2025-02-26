3df19eda9ba37c8eb3062e7df6ac7ad7b4c09428
leslie-smith1112 <leslie.smith1@ufl.edu>
2025-02-25 22:13:40 -0500

    missed this email

--- run_single.sh
+++ run_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=phyloFrame_run_single    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=leslie.smith1@ufl.edu     # Where to send mail
+#SBATCH --mail-user=<user email>     # Where to send mail
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=90gb
@@ -12,6 +12,6 @@
 
 ml R
 
-Rscript ./run_single.R thyroid TCGA_Thyroid_Gnomad4_cosmic_single_tester TCGA_eur_train_single_cos
-Rscript ./run_single.R uterine TCGA_Uterine_Gnomad4_cosmic_single_tester TCGA_eur_train_single_cos
-Rscript ./run_single.R breast TCGA_Breast_Gnomad4_cosmic_single_tester TCGA_eur_train_single_cos
+Rscript ./run_single.R thyroid TCGA_Thyroid_Gnomad4_single TCGA_eur_train
+Rscript ./run_single.R uterine TCGA_Uterine_Gnomad4_single TCGA_eur_train
+Rscript ./run_single.R breast TCGA_Breast_Gnomad4_single TCGA_eur_train
